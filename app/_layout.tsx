5ee364d548d9161cc0f5487f2a0125c822b354fc
Aamil13 <aamil.shafi13@gmail.com>
2025-11-29 22:11:51 +0530

    removed pre created expo files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,7 @@
 // import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useColorScheme } from "@/hooks/use-color-scheme";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+
 import React, { useState } from "react";
 import "react-native-reanimated";
 
@@ -16,7 +10,6 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [needsOnboarding, setNeedsOnboarding] = useState<boolean | null>(true);
   const [fontsLoaded] = useFonts({
     jumpsWinter: require("../assets/fonts/JumpsWinter.otf"),
@@ -34,18 +27,10 @@ export default function RootLayout() {
     return null; // or a loading indicator
   }
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        {needsOnboarding && (
-          <Stack.Screen name="onboarding" options={{ headerShown: false }} />
-        )}
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      {needsOnboarding && (
+        <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+      )}
+    </Stack>
   );
 }
